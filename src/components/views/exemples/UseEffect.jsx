fd2c49d25c2d6eec48f53f2c0a3ab744631efc27
genilson-dev <programationwork@gmail.com>
2025-04-29 21:38:13 -0300

    Add useEffect

--- src/components/views/exemples/UseEffect.jsx
+++ src/components/views/exemples/UseEffect.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
-// import PageTitle from '../../components/layout/PageTitle'
-import PageTitle from '../../layout/PageTitle'
+import React, { useEffect, useState } from "react";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+import SectionTitle from "../../layout/SectionTitle"
+import PageTitle from "../../layout/PageTitle";
+
+function calkFactory(num) {
+  let n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calkFactory(n - 1) * n;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [namber, setNamber] = useState(1);
+  const [factory, setFactory] = useState(1);
+
+  useEffect(() => {
+    setFactory(calkFactory(namber));
+  }, [namber]);
+
+  useEffect(
+    function () {
+      if (factory > 1000000) {
+        document.title = "Fatorial muito grande!";
+      }
+    },
+    [factory]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+        <SectionTitle title="Exercicio #01"/>
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">
+            {factory === -1 ? "Não Existe" : factory}
+          </span>
+        </div>
+        <div>
+          <input
+            type="number"
+            className="input"
+            value={namber}
+            onChange={(e) => setNamber(e.target.value)}
+          />
+        <SectionTitle title="Exercicio #02"/>
+          {/* <span className="text">{namber}</span> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
